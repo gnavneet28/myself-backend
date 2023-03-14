f7ee8a4e3069722084fc35ca951f380146410f5e
gnavneet28 <sggauravn@gmail.com>
2023-03-14 12:20:26 +0530

    added some more data

--- data.js
+++ data.js
@@ -1,8 +1,10 @@
 module.exports.myData = `My name is Gaurav kumar navneet. At first it was Gaurav but then i changed it. I was born on 1st of april
-so my friends make fun of me most of the time. I live with my family in hajipur, bihar india. I live with my mom, dad and my brother saurav.
+so my friends make fun of me most of the time. I live with my family in hajipur, bihar india. I live with my mom, dad and my
+ brother saurav.
 I am an entrepreneur and currently working on a project called CashYog that will help people a lot to save their money on the services they buy. I really hope this works.
 I really like gajar ka halwa, and everything cooked by my mother.My father is very caring and currently works in BSNL. I have a cousin sister whose name is Honey and she sends me memes everyday. She is hilarious.
 My brother is always busy with his girlfriend but he is a hardworking and smart person. I have done b-tech in mechanical engineering from vit vellore.
 My time in college was not that good but i am satisfied with my current situation. I do not have any girlfriend and i think i would be directly marrying someone my parents select for me.
 Anyway I hope I will make my parents proud of me. Honey eats a lot, but goes to gym regularly to balance the calories. She has a sistes, named Sweety and a brother named Satyam all are good people.
-Satyam always uses a funny line : Hag Mut lit chiya. I have many friend including Amit, Saurav, Ritesh, Ritu Raj and may more. They all have helped me a lot in my journey till now.`;
+Satyam always uses a funny line : Hag Mut lit chiya. I have many friend including Amit, Saurav, Ritesh, Ritu Raj and may more. They all have helped me a lot in my journey till now. I am also working on a project called Predesign with my senior Siddhart bhaiya who is a graphic designer.
+ Predesign has some tools that uses AI to create persona, site-map and a lot of design stuff. I usually hang out with my friends in the evening and enjoy tea. I donot smoke or drink and follow a healthy lifestyle`;
